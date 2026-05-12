55bef28e38ce9da6f396b7c69a6679951ee225ad
Bemi0001 <bemi0001@stud.kea.dk>
2026-05-12 03:19:17 +0200

    footer mobil version kodet

--- 3sem-eksamen/components/Footer.tsx
+++ 3sem-eksamen/components/Footer.tsx
@@ -15,8 +15,8 @@ export default function Footer() {
         
         <div className="grid grid-cols-1 gap-14 lg:grid-cols-[1.3fr_1fr_1fr] lg:gap-20">
           
-          <section>
-        <div className="mb-14">
+          <section className="text-center lg:text-left">
+        <div className="mb-25 flex justify-center lg:mb-14 lg:justify-start">
   <Link
     href="/"
     aria-label="Go to homepage"
@@ -27,18 +27,18 @@ export default function Footer() {
       alt="Night Club logo"
       width={180}
       height={90}
-      className="block h-auto w-[180px]"
+      className="block h-auto w-[270px] lg:w-[180px]"
       priority={false}
     />
   </Link>
 </div>
 
             <div className="mb-8">
-              <h3 className="mb-2 text-xl font-medium text-[oklch(0.65_0.25_8)]">
+              <h3 className="mb-3 text-3xl font-medium text-[oklch(0.65_0.25_8)] lg:mb-2 lg:text-xl">
                 LOCATION
               </h3>
 
-              <p className="text-base font-medium leading-8">
+              <p className="text-2xl font-medium leading-10 lg:text-base lg:leading-8">
                 Kompagnistræde 278
                 <br />
                 1265 København K
@@ -46,11 +46,11 @@ export default function Footer() {
             </div>
 
             <div>
-              <h3 className="mb-2 text-xl font-medium text-[oklch(0.65_0.25_8)]">
+              <h3 className="mb-3 text-3xl font-medium text-[oklch(0.65_0.25_8)] lg:mb-2 lg:text-xl">
                 OPENING HOURS
               </h3>
 
-              <p className="text-base font-medium leading-8">
+              <p className="text-2xl font-medium leading-10 lg:text-base lg:leading-8">
                 WED - THU 10:30 PM TO 3 AM
                 <br />
                 SAT - SUN: 11 PM TO 5 AM
@@ -149,46 +149,51 @@ export default function Footer() {
         </div>
 
         
-        <div className="mt-auto grid grid-cols-1 items-end gap-8 lg:grid-cols-3">
-          <p className="hidden text-sm font-medium text-[oklch(0.64_0_0)] lg:block">
-            Night Club - All Rights Reserved
-          </p>
+       <div className="mt-20 grid grid-cols-1 items-center gap-10 text-center lg:mt-auto lg:grid-cols-3 lg:items-end lg:gap-10">
+  <p className="order-2 text-lg font-medium leading-10 text-[oklch(0.64_0_0)] lg:order-1 lg:text-left lg:text-sm lg:leading-normal">
+    Night Club
+    <br className="lg:hidden" />
+    <span className="hidden lg:inline"> - </span>
+    All Rights Reserved
+  </p>
 
-          <div className="text-center">
-            <p className="mb-5 text-base font-medium">Stay Connected With Us</p>
+  <div className="order-1 text-center lg:order-2">
+    <p className="mb-7 text-2xl font-medium lg:mb-5 lg:text-base">
+      Stay Connected With Us
+    </p>
 
-            <div className="flex justify-center gap-5">
-              <a
-                href="#"
-                aria-label="Facebook"
-                className="flex h-11 w-11 items-center justify-center border-2 border-white text-sm transition hover:border-[oklch(0.65_0.25_8)] hover:text-[oklch(0.65_0.25_8)] focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-[oklch(0.65_0.25_8)]"
-              >
-               <FaFacebookF className="h-5 w-5" />
-              </a>
+    <div className="mb-10 flex justify-center gap-8 lg:mb-0 lg:gap-5">
+      <a
+        href="#"
+        aria-label="Facebook"
+        className="flex h-14 w-14 items-center justify-center border-2 border-white text-white transition hover:border-[oklch(0.65_0.25_8)] hover:text-[oklch(0.65_0.25_8)] focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-[oklch(0.65_0.25_8)] lg:h-11 lg:w-11"
+      >
+        <FaFacebookF className="h-6 w-6 lg:h-5 lg:w-5" />
+      </a>
 
-              <a
-                href="#"
-                aria-label="Snapchat"
-                className="flex h-11 w-11 items-center justify-center border-2 border-white text-sm transition hover:border-[oklch(0.65_0.25_8)] hover:text-[oklch(0.65_0.25_8)] focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-[oklch(0.65_0.25_8)]"
-              >
-                <FaSnapchatGhost className="h-5 w-5" />
-              </a>
+      <a
+        href="#"
+        aria-label="Snapchat"
+        className="flex h-14 w-14 items-center justify-center border-2 border-white text-white transition hover:border-[oklch(0.65_0.25_8)] hover:text-[oklch(0.65_0.25_8)] focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-[oklch(0.65_0.25_8)] lg:h-11 lg:w-11"
+      >
+        <FaSnapchatGhost className="h-6 w-6 lg:h-5 lg:w-5" />
+      </a>
 
-              <a
-                href="#"
-                aria-label="Instagram"
-                className="flex h-11 w-11 items-center justify-center border-2 border-white text-sm transition hover:border-[oklch(0.65_0.25_8)] hover:text-[oklch(0.65_0.25_8)] focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-[oklch(0.65_0.25_8)]"
-              >
-                <FaInstagram className="h-5 w-5" />
-              </a>
-            </div>
-          </div>
+      <a
+        href="#"
+        aria-label="Instagram"
+        className="flex h-14 w-14 items-center justify-center border-2 border-white text-white transition hover:border-[oklch(0.65_0.25_8)] hover:text-[oklch(0.65_0.25_8)] focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-[oklch(0.65_0.25_8)] lg:h-11 lg:w-11"
+      >
+        <FaInstagram className="h-6 w-6 lg:h-5 lg:w-5" />
+      </a>
+    </div>
+  </div>
 
-          <p className="hidden text-right text-sm font-medium text-[oklch(0.64_0_0)] lg:block">
-            Copyright © NightClub
-          </p>
-        </div>
-      </div>
+  <p className="order-3 text-lg font-medium text-[oklch(0.64_0_0)] lg:text-right lg:text-sm">
+    Copyright © NightClub
+  </p>
+</div>
+ </div>
     </footer>
   );
 }
