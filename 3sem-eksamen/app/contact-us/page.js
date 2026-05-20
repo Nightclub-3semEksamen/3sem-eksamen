f259a4d9704f76692768aa278ca0c70a5a82899a
maha0034 <maha0034@stud.kea.dk>
2026-05-20 12:12:01 +0200

    fix

--- 3sem-eksamen/app/contact-us/page.js
+++ 3sem-eksamen/app/contact-us/page.js
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
-
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export default function ContactPage() {
@@ -84,8 +83,8 @@ export default function ContactPage() {
 
   return (
     <main className="bg-black min-h-screen text-white overflow-hidden">
-      {/* HERO */}
       <Navbar />
+      {/* HERO */}
       <section
         className="relative h-[160px] md:h-[240px] bg-cover bg-center flex items-center justify-center"
         style={{
